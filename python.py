9443e5b3d51bccb96160fdf95a4d495db0e2f611
Satyakvv108 <156512368+Satyakvv108@users.noreply.github.com>
2024-04-30 12:38:50 +0530

    Add files via upload

--- python.py
+++ python.py
@@ -1,6 +1,6 @@
 def greet():
-    name = input("Enter your name: ")
-    print("Hello, {}! Welcome to the Python script.".format(name))
+    print("Hello! Welcome to the Python script.")
 
 if __name__ == "__main__":
     greet()
+
